b07e5c02f6852081cbfe0cfc91755c033cca3ee3
samuel93250 <sam.izbo@gmail.com>
2018-11-05 18:03:53 +0100

    :sparkles: Add nom de la feature rajoutée

--- scripts/app.js
+++ scripts/app.js
@@ -29,6 +29,7 @@ shooter.$start = shooter.$container.querySelector('.start')
 shooter.$score = shooter.$container.querySelector('.score .value')
 shooter.$timer = shooter.$container.querySelector('.timer')
 shooter.$targets = shooter.$container.querySelector('.targets')
+shooter.score = 0 // pas de $ car pas element DOM
 
 /**
  * Methods
@@ -40,6 +41,25 @@ shooter.addTarget = () =>
     $target.style.top = `${Math.random() * 100}%` // placement aléatoire
     $target.style.left = `${Math.random() * 100}%` // placement aléatoire
     shooter.$targets.appendChild($target)
+
+    // Listen to mouse enter
+    $target.addEventListener('mouseenter', () => //on met l'event directement dans la fonction
+    {
+        shooter.shootTarget($target) //pour ne pas avoir + de 10 instructions dans la fonction, on met appel une fonction dans une fonction
+    })
+}
+
+shooter.shootTarget = (_$target) => // On met un underscore car c'est un parametre de fonction
+{
+    // Delete target
+    _$target.remove() //enlever element html
+    // Add new target
+    shooter.addTarget() //on rappelle la fonction pour remttre des cibles aléatoirement
+    // Increment score
+    shooter.score++
+    shooter.$score.textContent = shooter.score
+
+    // Play sound
 }
 
 shooter.addTarget()
